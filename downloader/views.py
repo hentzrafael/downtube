f9b48b515f672fe4d41906a205938294c8a00ace
hentzrafael <rafaelhentz172@gmail.com>
2023-11-23 15:36:16 -0300

    Added django

---
+++ downloader/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+from django.template import loader
+# Create your views here.
+def index(request):
+    template = loader.get_template('downloader/index.html')
+    return HttpResponse(template.render({},request))
--- main.py
+++ main.py
@@ -1,14 +1,4 @@
 from pytube import Playlist, YouTube
-"""
-    Downloads a video from a given URL and saves it to the specified path.
-
-    Parameters:
-        url (str): The URL of the video to download.
-        path (str): The path where the downloaded video will be saved.
-
-    Returns:
-        None
-"""
 def download(url, path):
     yt = Playlist(url)
     for item in yt.video_urls:
