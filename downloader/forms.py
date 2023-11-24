bfc9ec8e7b07818e16c9b9ac8fedc16e8ebe3474
hentzrafael <rafaelhentz172@gmail.com>
2023-11-24 15:27:42 -0300

    Added only audio download

--- downloader/forms.py
+++ downloader/forms.py
@@ -2,3 +2,4 @@ from django import forms
 
 class UrlForm(forms.Form):
     url = forms.URLField(label="Youtube Playlist URL",max_length=200)
+    is_audio = forms.BooleanField(label="Audio Only",initial=False, required=False)
--- downloader/views.py
+++ downloader/views.py
@@ -3,21 +3,27 @@ from django.template import loader
 from django.shortcuts import render
 from .forms import UrlForm
 from pytube import Playlist, YouTube
+import time
 
 def index(request):
     if request.method == 'POST':
         form = UrlForm(request.POST)
         if form.is_valid():
             url = form.cleaned_data['url']
+            only_audio = form.cleaned_data['is_audio']
+            print(only_audio)
             #TODO: download the video
-            download_playlist(url, "./tmp")
+            download_playlist(url, "./tmp",only_audio=form.cleaned_data['is_audio'])
     else:
         form = UrlForm()
     return render(request,'downloader/index.html', {'form': form})
 
-def download_playlist(url, path):
+def download_playlist(url, path,only_audio=False):
     yt = Playlist(url)
     for item in yt.video_urls:
         yout = YouTube(item)
-        yout.streams.filter(only_audio=False,only_video=False,progressive=True).get_highest_resolution().download(path)
+        if only_audio:
+            yout.streams.get_audio_only().download(path, filename_prefix=str(time.time()))
+        else:
+            yout.streams.filter(only_audio=False,only_video=False,progressive=True).get_highest_resolution().download(path, filename_prefix=str(time.time()))
 
