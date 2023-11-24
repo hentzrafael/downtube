c5a31325f2fdfb6b82b77a43cf17daed0bac85fd
hentzrafael <rafaelhentz172@gmail.com>
2023-11-24 14:58:13 -0300

    Added Form to capture YouTube playlist URL

--- downloader/views.py
+++ downloader/views.py
@@ -1,6 +1,23 @@
 from django.http import HttpResponse
 from django.template import loader
-# Create your views here.
+from django.shortcuts import render
+from .forms import UrlForm
+from pytube import Playlist, YouTube
+
 def index(request):
-    template = loader.get_template('downloader/index.html')
-    return HttpResponse(template.render({},request))
+    if request.method == 'POST':
+        form = UrlForm(request.POST)
+        if form.is_valid():
+            url = form.cleaned_data['url']
+            #TODO: download the video
+            download_playlist(url, "./tmp")
+    else:
+        form = UrlForm()
+    return render(request,'downloader/index.html', {'form': form})
+
+def download_playlist(url, path):
+    yt = Playlist(url)
+    for item in yt.video_urls:
+        yout = YouTube(item)
+        yout.streams.filter(only_audio=False,only_video=False,progressive=True).get_highest_resolution().download(path)
+
