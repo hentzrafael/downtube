17ef08f8b8591522ec962278e6a20c86987763c3
hentzrafael <rafaelhentz172@gmail.com>
2023-11-27 13:40:10 -0300

    Remove field of url type from form

--- downloader/forms.py
+++ downloader/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 
 class UrlForm(forms.Form):
-    url = forms.URLField(label="Youtube Playlist URL",max_length=200)
-    tipo = forms.ChoiceField(widget=forms.RadioSelect, choices=[(1, 'Playlist'), (2, 'Video')],label="Escolha o tipo:",required=True)
-    audio_or_video = forms.ChoiceField(widget=forms.RadioSelect, choices=[(True, 'Audio'), (False, 'Video')],label="Escolha o tipo do arquivo:",required=False)
+    url = forms.URLField(label="Youtube URL",max_length=200,required=True,widget=forms.TextInput(attrs={'class': 'form-control'}))
+    audio_or_video = forms.ChoiceField(widget=forms.RadioSelect(attrs={'class': 'form-check'}), choices=[(True, 'Audio'), (False, 'Video')],label="Escolha o tipo do arquivo:",required=True)
--- downloader/views.py
+++ downloader/views.py
@@ -11,28 +11,34 @@ def index(request):
         if form.is_valid():
             url = form.cleaned_data['url']
             audio_or_video = form.cleaned_data['audio_or_video']
-            tipo = form.cleaned_data['tipo']
-            if tipo == '1':
+            if 'playlist' in url:
                 download_playlist(url, "./tmp",only_audio=audio_or_video)
-            elif tipo == '2':
+            else:
                 download_video(url, "./tmp",only_audio=audio_or_video)
-            
     else:
         form = UrlForm()
     return render(request,'downloader/index.html', {'form': form})
 
+def on_progress(stream, chunk, bytes_remaining):
+    total_bytes = stream.filesize
+    bytes_downloaded = total_bytes - bytes_remaining
+    progress = (bytes_downloaded / total_bytes) * 100
+    print(f"Download progress: {progress:.2f}%")
+    return f"<script>updateProgressBar({progress})</script>"
+
 def download_playlist(url, path,only_audio=False):
     yt = Playlist(url)
     for item in yt.video_urls:
-        yout = YouTube(item)
+        yout = YouTube(item,on_progress_callback=on_progress)
         if only_audio == "True":
-            yout.streams.get_audio_only().download(path, filename_prefix=str(time.time()),filename="audio.mp3")
+            yout.streams.get_audio_only().download(path,filename=f"{yout.title}.mp3")
         else:
-            yout.streams.filter(only_audio=False,only_video=False,progressive=True).get_highest_resolution().download(path, filename_prefix=str(time.time()))
+            yout.streams.filter(only_audio=False,only_video=False,progressive=True).get_highest_resolution().download(path)
 
 def download_video(url, path,only_audio=False):
     yout = YouTube(url)
+    yout.register_on_progress_callback(on_progress)
     if only_audio == "True":
-        yout.streams.get_audio_only().download(path, filename_prefix=str(time.time()),filename="audio.mp3")
+        yout.streams.get_audio_only().download(path,filename=f"{yout.title}.mp3")
     else:
-        yout.streams.filter(only_audio=False,only_video=False,progressive=True).get_highest_resolution().download(path, filename_prefix=str(time.time()))
+        yout.streams.filter(only_audio=False,only_video=False,progressive=True).get_highest_resolution().download(path)
